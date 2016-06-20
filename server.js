571408cf67185e366ad3a0e79b7cab8e1a465f4c
sachin03jain <sachin.jain@learningmate.com>
2016-06-20 15:05:46 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http')
+    var port = process.env.PORT || 3000;
+    var host = process.env.HOST || '0.0.0.0';
+    http.createServer(function(req, res) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Hello World!\n');
+    }).listen(port, host);
+    console.log("Server running at "+host+":"+port);
