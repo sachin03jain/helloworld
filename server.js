05a55d634c3f512c4f1354138cf9aa2e0122e9e6
sachin03jain <sachin.jain@learningmate.com>
2016-06-20 19:31:30 +0530

    added add method

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var express = require('express');
 
  var app = express();
  var server = http.createServer(app);
+ var value = 0;
 
-    var port = process.env.PORT || 3000;
-    var host = process.env.HOST || '0.0.0.0';
+ var port = process.env.PORT || 3000;
+ var host = process.env.HOST || '0.0.0.0';
    /* http.createServer(function(req, res) {
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end('Hello World!\n Server running at '+host+':'+port);
@@ -23,3 +24,16 @@ var express = require('express');
    		res.writeHead(200, { 'Content-Type': 'application/json' });
       	res.end(JSON.stringify({data:"hello world! getData"}));
    });
+
+    app.get('/add/:a/:b',function(req,res){
+    	try{
+    		var a = parseInt(req.params.a);
+    		var b = parseInt(req.params.b);
+    		res.end(JSON.stringify({data:a+b}));
+    	}catch(ex){
+    		
+    		res.end(JSON.stringify({data:ex}));
+    	}
+    	
+
+    });
