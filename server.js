6577b3bd3ec5e04d582233ba635e22788fa12078
sachin03jain <sachin.jain@learningmate.com>
2016-06-20 17:10:08 +0530

    third commit

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-var http = require('http')
-    var port = process.env.PORT || 3000;
+var http = require('http');
+ var express = require('express');
+
+ var app = express();
+ var server = http.createServer(app);
+
+   /* var port = process.env.PORT || 3000;
     var host = process.env.HOST || '0.0.0.0';
     http.createServer(function(req, res) {
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end('Hello World!\n Server running at '+host+':'+port);
-    }).listen(port, host);
-   
+    }).listen(port, host);*/
+   server.listen(port,function(){
+
+   });
+
+   app.get('/',function(req,res){
+   		res.writeHead(200, { 'Content-Type': 'text/plain' });
+      	res.end('Hello World!\n Server running get service ');
+   });
