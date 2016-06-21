9341f760fc4f3e8ff290557ebb6973a4c30b91ab
sachin03jain <sachin.jain@learningmate.com>
2016-06-21 15:51:04 +0530

    change in app.js

--- public/app.js
+++ public/app.js
@@ -12,6 +12,7 @@ $(function()
 
               $.post('https://functions1daafd45.azurewebsites.net/api/deepTest?code=hn4o9o6flk64p1eemzfz7vzgno9n1h9p',data).success(function(response) {
 
+                 alert("response "+ response.val);
               if(response){
                 if(response.status==200){
                   alert("response"+ response.val);
@@ -35,6 +36,7 @@ $(function()
 
         $.post('https://functions1daafd45.azurewebsites.net/api/fetchDataFromSql2?code=ts760dcv9rp3c6czspdtrsh3jvlds9i').success(function(response) {
            
+            alert("response "+ response.val);
           if(response){
               if(response.status==200){
                  alert("response "+ response.val);
