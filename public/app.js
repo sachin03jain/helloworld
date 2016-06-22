700650f06f8bfd180b09d2994d14b24a8a9195c8
sachin03jain <sachin.jain@learningmate.com>
2016-06-22 15:31:31 +0530

    added code to show list

--- public/app.js
+++ public/app.js
@@ -65,12 +65,9 @@ $(function()
              console.log("error");
          });
       });*/
-       var params = {
-            // Request parameters
-        };
       
         $.ajax({
-            url: "https://sjapimanagement.azure-api.net/caliper/get/caliper/get?" + $.param(params),
+            url: "https://sjapimanagement.azure-api.net/caliper/get/caliper/get?",
             beforeSend: function(xhrObj){
                 // Request headers
                 xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","242c0bbeeee44f72adb0");
@@ -85,7 +82,7 @@ $(function()
         .fail(function() {
             alert("error");
         });
+    });
   });
-
 
 });
